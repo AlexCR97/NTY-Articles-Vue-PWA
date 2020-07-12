e89760d6bfd584903dc490af697dc1e0c2f47ac7
AlexCR97 <carp_97@outlook.com>
2020-07-11 23:38:26 -0500

    Initial commit

---
+++ precache-manifest.d63cc87b0b505eb5ac64f460c57d792d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df38c17a44dcd914d705",
+    "url": "/css/app.6927d382.css"
+  },
+  {
+    "revision": "3193acb1af8e8d442075",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "38e11da72b9d4436451d36df0a5b6f2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df38c17a44dcd914d705",
+    "url": "/js/app.5864da5f.js"
+  },
+  {
+    "revision": "3193acb1af8e8d442075",
+    "url": "/js/chunk-vendors.50a8f3cc.js"
+  },
+  {
+    "revision": "a4b8b5941790bffd1a181116b5aeb090",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
